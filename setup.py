ffc8c13faf625a6f3a878ef67c967294af71db36
chaipokoi <chaipokoi@gmail.com>
2016-07-17 22:20:06 +0200

    Ajout d'un fichier de config pour openShift

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Pokemon-Go-WebAPI',
+      version='1.0',
+      description='',
+      author='Clovis Portron',
+      author_email='portron.cl@gmail.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=[ 'bottle' ],
+)
