dcb4f3c18a848475e7e8be6f6a9af2af85f02280
chaipokoi <chaipokoi@gmail.com>
2016-07-19 10:31:40 +0200

    Mise au point du lancement de requete

--- js/embed.js
+++ js/embed.js
@@ -6,13 +6,38 @@ function parseQuery()
 {
     var regex = new RegExp("params=(.*)");
     var res = regex.exec(window.location.href);
-    return res
+    return res[1]
 }
+
+/**
+ * Examine le résultat d'une requete ajax
+ */
+function parseResponse(result)
+{
+    document.write(result);  
+}
+
+/* 
+ * Démarre une requete AJAX à l'adresse spécifiée
+ */
+function startRequest(url)
+{
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            parseResponse(xhttp.responseText);
+        }
+    };
+    console.log("Processing "+url);
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
 
 
 window.onload = function()
 {
     var params = parseQuery();
-    document.write(params)
+    startRequest("http://pokemon-chaipokoi.rhcloud.com/api?params="+params);
 }
 
