3d7c1cd4c05eb13dfafb6a6b9503a061a960ef6b
chaipokoi <chaipokoi@gmail.com>
2016-07-21 22:13:55 +0200

    Avancement de la génération de cartes

---
+++ js/index.js
@@ -0,0 +1,49 @@
+/**
+ * Raccourcis pour getElementById
+ */
+function node(id)
+{
+    return document.getElementById(id);
+}
+
+/**
+ * Obtient le data token et produit le code de carte  
+ */
+function processStart()
+{
+    //récupération de la clef 
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            processEnd(xhttp.responseText);
+        }
+        else if(xhttp.readyState == 4)
+        {
+            showError(node("errors"), "Network or server error, please check your credentials and try again later.");
+        }
+    };
+    xhttp.open("GET", "https://pokemon-chaipokoi.rhcloud.com/pubkey", true);
+    xhttp.send();
+}
+
+function processEnd(key)
+{
+    key = JSON.parse(key).pubkey;
+    key = key.replace(/\n/g, "");
+    console.log(key);
+    var username = node("username").value;
+    var password = node("password").value;
+    var credentials = username+"&"+password;
+
+    // Create the encryption object and set the key.
+    var crypt = new JSEncrypt();
+    crypt.setKey(key); //You can use also setPrivateKey and setPublicKey, they are both alias to setKey
+
+    var token = crypt.encrypt(credentials);
+    console.log(token);
+}
+
+window.onload = function()
+{
+    node("process").addEventListener("click", processStart);
+}
