daeb2f06ce74239a2257fb7cb6468801aa79af30
chaipokoi <chaipokoi@gmail.com>
2016-07-22 10:51:40 +0200

    Tentative de résolution du problem de clef

--- js/index.js
+++ js/index.js
@@ -29,17 +29,21 @@ function processStart()
 function processEnd(key)
 {
     key = JSON.parse(key).pubkey;
-    key = key.replace(/\n/g, "");
-    console.log(key);
+    //key = key.replace(/\n/g, "");
     var username = node("username").value;
     var password = node("password").value;
     var credentials = username+"&"+password;
 
-    // Create the encryption object and set the key.
-    var crypt = new JSEncrypt();
-    crypt.setKey(key); //You can use also setPrivateKey and setPublicKey, they are both alias to setKey
+    key = "-----BEGIN PUBLIC KEY-----\
+AAAAB3NzaC1yc2EAAAADAQABAAABAQDOUwbPkzLjcfyffjdOiOMygM/HEii2tQ3LAH425gFhVY7Z/TvvTKaUwvmwMPyKfIgZOwT8z9TiZOM8wABT8Niq/yje3F42AtYy0iQxKg7eVMgZh6QrFHAeOP8rfmWuhw0M0W0HmpyEhCeGpF9HnRzF9WozJOFQYatRoiNzh0V5nwBIcNdO010wHzlbrfAl0wEx4UdhZaW+18kbkNlcZKO0VHU9bRXUQ+aDRxX1gK/stWF1/XOhfIHa24ME+R1VNRqS+ku9TymPn4CDlzK7OfcFXt5yYBKc1th89FlO2w9LFx4ApogzDya1kpfqIQ50oVHQjGbKf7o5NXazJ8KUgzKp\
+-----END PUBLIC KEY-----";
+    
 
-    var token = crypt.encrypt(credentials);
+    console.log(key);
+
+    key = RSA.getPublicKey(key);
+    console.log(key);
+    var token = RSA.encrypt(credentials, key);
     console.log(token);
 }
 
