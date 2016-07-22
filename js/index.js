00cfb343f9ac586db0fad289d7b4f6ece1012638
chaipokoi <chaipokoi@gmail.com>
2016-07-22 20:09:44 +0200

    avancement dans generateur

--- js/index.js
+++ js/index.js
@@ -29,21 +29,20 @@ function processStart()
 function processEnd(key)
 {
     key = JSON.parse(key).pubkey;
-    //key = key.replace(/\n/g, "");
+    key = key.replace(/\n/g, "");
     var username = node("username").value;
     var password = node("password").value;
     var credentials = username+"&"+password;
-
-    key = "-----BEGIN PUBLIC KEY-----\
-AAAAB3NzaC1yc2EAAAADAQABAAABAQDOUwbPkzLjcfyffjdOiOMygM/HEii2tQ3LAH425gFhVY7Z/TvvTKaUwvmwMPyKfIgZOwT8z9TiZOM8wABT8Niq/yje3F42AtYy0iQxKg7eVMgZh6QrFHAeOP8rfmWuhw0M0W0HmpyEhCeGpF9HnRzF9WozJOFQYatRoiNzh0V5nwBIcNdO010wHzlbrfAl0wEx4UdhZaW+18kbkNlcZKO0VHU9bRXUQ+aDRxX1gK/stWF1/XOhfIHa24ME+R1VNRqS+ku9TymPn4CDlzK7OfcFXt5yYBKc1th89FlO2w9LFx4ApogzDya1kpfqIQ50oVHQjGbKf7o5NXazJ8KUgzKp\
------END PUBLIC KEY-----";
     
 
-    console.log(key);
 
-    key = RSA.getPublicKey(key);
-    console.log(key);
-    var token = RSA.encrypt(credentials, key);
+    var encrypt = new JSEncrypt();
+    encrypt.setPublicKey(key);
+    var token = encrypt.encrypt(credentials);
+    //token = token.replace(/\+/g, '-').replace(/\//g, '_');//conversion en url safe
+    token = atob(token);
+    console.log(token);
+    token = btoa("blah");
     console.log(token);
 }
 
