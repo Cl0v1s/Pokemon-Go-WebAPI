59fe987c66efc386bcde1b354d8376f316216de4
chaipokoi <chaipokoi@gmail.com>
2016-07-28 14:02:35 +0200

    Mise à jour du moteur

--- pgoapi/__init__.py
+++ pgoapi/__init__.py
@@ -1,6 +1,27 @@
 
-import os
-import sys
+from __future__ import absolute_import
 
-# add directory of this file to PATH, so that the package will be found
-sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+from pgoapi.exceptions import PleaseInstallProtobufVersion3
+
+import pkg_resources
+
+protobuf_exist = False
+protobuf_version = 0
+try:
+    protobuf_version = pkg_resources.get_distribution("protobuf").version
+    protobuf_exist = True
+except:
+    pass
+
+if (not protobuf_exist) or (int(protobuf_version[:1]) < 3):
+    raise PleaseInstallProtobufVersion3()
+
+from pgoapi.pgoapi import PGoApi
+from pgoapi.rpc_api import RpcApi
+from pgoapi.auth import Auth
+
+try:
+    import requests.packages.urllib3
+    requests.packages.urllib3.disable_warnings()
+except:
+    pass
