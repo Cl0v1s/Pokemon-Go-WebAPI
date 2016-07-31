e291ae119f3e568038c61ba5d64c4fb42f6c1966
chaipokoi <chaipokoi@gmail.com>
2016-07-31 13:08:54 +0200

    Ebauche de l'adaptation

--- js/embed.js
+++ js/embed.js
@@ -53,25 +53,54 @@ function parseResponse(result)
         return;
     }
     //Affichage des statistiques du joueur
-    var player = data["player"];
+    var player = data["results"]["responses"]["GET_PLAYER"]["player_data"];
+    console.log(player);
     showInfo(node("stat"), "Username", player["username"]);
-    var since = new Date(player["since"]);
+    var since = new Date(player["creation_timestamp_ms"]);//affichage de la date de debut de jeu
     since = since.getMonth() + "/" + since.getDate() + "/"+since.getFullYear();
     showInfo(node("stat"), "Play since", since);
-    showInfo(node("stat"), "Pokecoin", player["pokecoin"]);
-    showInfo(node("stat"), "Stardust", player["stardust"]);
+    var currencies = player["currencies"]; //affichage des monnaies
+    for(var i = 0; i != currencies.length; i++)
+    {
+        var currency = currencies[i];
+        var amount = 0;
+        if(currency["amount"] != undefined)
+            amount = currency["amount"];
+        showInfo(node("stat"), currency["name"], amount);
+    }
+    var player = data["results"]["responses"]["GET_INVENTORY"]["inventory_delta"]["inventory_items"]; //recherche des stats du joueur dans son inventaire
+    for(var i = 0; i != player.length; i++)
+    {
+        var item = player[i]["inventory_item_data"];
+        if(item["player_stats"] != undefined)
+        {
+            player = item["player_stats"];
+            break;
+        }
+    }
     showInfo(node("stat"), "Level", player["level"]);
-    showInfo(node("stat"), "Experience", player["xp"] + "/" + player["next_xp"]);
-    showInfo(node("stat"), "Capture rate", player["captured"] + "/" + player["encountered"]);
-    showInfo(node("stat"), "Pokedex", player["pokedex"]);
+    showInfo(node("stat"), "Experience", player["experience"] + "/" + player["next_level_xp"]);
+    showInfo(node("stat"), "Capture rate", player["pokemons_captured"] + "/" + player["pokemons_encountered"]);
+    showInfo(node("stat"), "Pokedex", player["unique_pokedex_entries"]);
 
     //Affichage des pokemons du joueur
-    var team = player["team"];
+    var team = new Array();
+    var teamData = data["results"]["responses"]["GET_INVENTORY"]["inventory_delta"]["inventory_items"]; //récupération des pokemons
+    for(var i = 0; i != player.length; i++)
+    {
+        var item = teamData[i]["inventory_item_data"];
+        if(item["pokemon_data"] != undefined)
+        {
+            //récupération du sprite
+            
+            team.push(item["pokemon_data"])
+        }
+    }
     team.sort(function(a,b) //tri du plus récent au plus vieux
     {
-        if(a.capture_date < b.capture_date)
+        if(a["creation_time_ms"] < b["creation_time_ms"])
             return 1;
-        else if(b.capture_date < a.capture_date)
+        else if(b["creation_time_ms"] < a["creation_time_ms"])
             return -1;
         else return 0;
     });
@@ -112,6 +141,6 @@ function startRequest(url)
 window.onload = function()
 {
     var params = parseQuery();
-    startRequest("https://pokemon-chaipokoi.rhcloud.com/api?params="+params);
+    startRequest("https://localhost:8080/api?params="+params+"&requests=get_player,get_inventory");
 }
 
